f33abe437564c70ce41160495d83b6e7f8e825d0
khanaliahmad296 <khanaliahmad296@gmail.com>
2025-01-08 11:46:06 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Generative Ai Project',
+    version= '0.0.0',
+    author= 'Ali Ahmad Khan',
+    author_email = "khanaliahmad296@gmail.com",
+    packages= find_packages(),
+    install_requires= []
+
+)
--- template.py
+++ template.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
-
+ 
 list_of_files=[
     "src/__init__.py",
     "src/helper.py",
